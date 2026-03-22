13f46215391e637a2d629a0bb198bd2ca3eca78c
rajneesh310 <bandwalrajneesh@gmail.com>
2026-03-22 10:35:19 +0000

    Add basic calculator functionality with menu options

--- assignment2.c
+++ assignment2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int choice;
+    float a, b;
+
+    printf("MENU\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    switch(choice) {
+        case 1:
+            printf("Addition = %.2f", a + b);
