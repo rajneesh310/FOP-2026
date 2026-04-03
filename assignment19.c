db53614a6eb5365c606787a57903b929ea6b66dc
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-03 05:40:23 +0000

    Refactor employee management program to improve structure and functionality

--- assignment19.c
+++ assignment19.c
@@ -1,69 +1,91 @@
-//create structure EMPLOYEE for storing details (name,designation,gender,,date of joining and salary). define function members to compute a) total no. ofnemployees in an organisation b) count of male and female employee c) employee with salary more than 10,000 d) employee with designation "asst mnager"
 #include <stdio.h>
-#include <string.h>             
+#include <string.h>
 
 struct employee {
     char name[50];
     char designation[50];
-    char gender[10];
-    char date_of_joining[20];
+    char gender;
+    char doj[20];   // date of joining
     float salary;
-};              
+};
 
+// Function to count total employees
+void totalEmployees(int n) {
+    printf("\nTotal number of employees: %d\n", n);
+}
 
+// Function to count male and female employees
+void countGender(struct employee e[], int n) {
+    int male = 0, female = 0, i;
+
+    for (i = 0; i < n; i++) {
+        if (e[i].gender == 'M' || e[i].gender == 'm')
+            male++;
+        else if (e[i].gender == 'F' || e[i].gender == 'f')
+            female++;
+    }
+
+    printf("\nMale Employees: %d", male);
+    printf("\nFemale Employees: %d\n", female);
+}
+
+// Function to display employees with salary > 10000
+void highSalary(struct employee e[], int n) {
+    int i;
+    printf("\nEmployees with salary more than 10000:\n");
+
+    for (i = 0; i < n; i++) {
+        if (e[i].salary > 10000) {
+            printf("%s\n", e[i].name);
+        }
+    }
+}
+
+// Function to display employees with designation "asst manager"
+void asstManager(struct employee e[], int n) {
+    int i;
+    printf("\nEmployees with designation 'asst manager':\n");
+
+    for (i = 0; i < n; i++) {
+        if (strcmp(e[i].designation, "asst manager") == 0) {
+            printf("%s\n", e[i].name);
+        }
+    }
+}
 
 int main() {
     int n, i;
-    int total_employees = 0;
-    int male_count = 0;             
-    int     female_count = 0;
-    int salary_above_10000 = 0;
-    int asst_manager_count = 0; 
 
     printf("Enter number of employees: ");
-    scanf("%d", &n);    
+    scanf("%d", &n);
 
-    struct employee emp[n];
+    struct employee e[n];
+
+    // Input employee details
     for (i = 0; i < n; i++) {
         printf("\nEnter details of employee %d\n", i + 1);
 
         printf("Name: ");
-        scanf("%s", emp[i].name);
+        scanf("%s", e[i].name);
 
         printf("Designation: ");
-        scanf("%s", emp[i].designation);
+        scanf("%s", e[i].designation);
 
-        printf("
-Gender: ");
-        scanf("%s", emp[i].gender);
+        printf("Gender (M/F): ");
+        scanf(" %c", &e[i].gender);
+
         printf("Date of Joining: ");
-        scanf("%s", emp[i].date_of_joining);
+        scanf("%s", e[i].doj);
+
         printf("Salary: ");
-        scanf("%f", &emp[i].salary);
-            
-        total_employees++;
-        if (strcmp(emp[i].gender, "male") == 0) {   
-
-
-
-            male_count++;        } else if (strcmp(emp[i].gender, "female") == 0) { 
-                female_count++;
-            }
-        }
-        if (emp[i].salary > 10000) {
-            salary_above_10000++;
-        }
-        if (strcmp(emp[i].designation, "asst manager") == 0) {
-            asst_manager_count++;
-        }
+        scanf("%f", &e[i].salary);
     }
 
-    printf("\nTotal number of employees: %d\n", total_employees);
-    printf("Count of male employees: %d\n", male_count);
-    printf("Count of female employees: %d\n", female_count);
-    printf("Employees with salary more than 10,000: %d\n", salary_above_10000);
-    printf("Employees with designation 'asst manager': %d\n", asst_manager_count);
+    // Function calls
+    totalEmployees(n);
+    countGender(e, n);
+    highSalary(e, n);
+    asstManager(e, n);
 
     return 0;
-}   
-    
+}
