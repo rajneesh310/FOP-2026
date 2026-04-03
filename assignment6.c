53559ce401353b995a13d2e0b5813de61e2e1be1
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-03 05:21:51 +0000

    Add factorial calculation program with recursive and iterative methods

---
+++ assignment6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Recursive function
+int factorial_recursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;  // Base case
+    else
+        return n * factorial_recursive(n - 1);
+}
+
+// Iterative function
+int factorial_iterative(int n) {
+    int i, fact = 1;
+    for (i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial (Iterative) = %d\n", factorial_iterative(num));
+        printf("Factorial (Recursive) = %d\n", factorial_recursive(num));
+    }
+
+    return 0;
+}
