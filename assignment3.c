10ded31a1eb938e079302ca2d10937d4a7496e89
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:25:20 +0000

    Remove unnecessary comments for clarity in multiplication table program

--- assignment3.c
+++ assignment3.c
@@ -3,11 +3,11 @@
 int main() {
     int num, i;
 
-    // Input from user
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Generate multiplication table
+    
     printf("Multiplication Table of %d:\n", num);
 
     for(i = 1; i <= 10; i++) {
