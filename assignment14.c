d92d711af0c702ab886e135181dd69bb4aa83c41
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:31:42 +0000

    kgk

--- assignment14.c
+++ assignment14.c
@@ -4,23 +4,20 @@ int main() {
     int num1, num2, i, smallestCD = -1, gcd;
     int a, b, temp;
 
-    // Input: Accept two numbers from the user [cite: 4, 23]
+    
     printf("Enter Number 1: ");
     scanf("%d", &num1);
     printf("Enter Number 2: ");
     scanf("%d", &num2);
 
-    // Part 1: Finding the Smallest Common Divisor (other than 1) [cite: 5, 14]
-    // We start from 2 and check each number up to the smaller of the two inputs
+    
     int limit = (num1 < num2) ? num1 : num2;
     for (i = 2; i <= limit; i++) {
         if (num1 % i == 0 && num2 % i == 0) {
             smallestCD = i;
-            break; // Stop at the first (smallest) common divisor found
-        }
-    }
+            break; 
 
-    // Part 2: Finding the Greatest Common Divisor (GCD) using Euclidean Algorithm [cite: 6, 16]
+    
     a = num1;
     b = num2;
     while (b != 0) {
@@ -30,7 +27,7 @@ int main() {
     }
     gcd = a;
 
-    // Output: Display results 
+    
     printf("\n--- Results ---\n");
     if (smallestCD != -1) {
         printf("Smallest Common Divisor: %d\n", smallestCD);
