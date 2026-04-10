4a671bbcd2fe3ac750a95b2e6cd3d693b4933aa4
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:29:01 +0000

    tgjdrhdrrj

--- assignment9.c
+++ assignment9.c
@@ -9,14 +9,14 @@ int main() {
 
     original = num;
 
-    // Extract digits and calculate sum of cubes
+    
     while (num > 0) {
         remainder = num % 10;
         sum = sum + (remainder * remainder * remainder);
         num = num / 10;
     }
 
-    // Check Armstrong condition
+    
     if (sum == original) {
         printf("%d is an Armstrong number.\n", original);
     } else {
