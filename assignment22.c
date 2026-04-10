630c34e2d29c159fc43f72efcfc14a4b6b0f9c7d
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:37:51 +0000

    waegtjily

--- assignment22.c
+++ assignment22.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <string.h> // Required for built-in string functions [cite: 72]
+#include <string.h> 
 
 int main() {
     char str1[100], str2[100], temp[200];
     int choice;
 
     printf("Enter first string: ");
-    scanf("%s", str1); // [cite: 81]
+    scanf("%s", str1); 
     printf("Enter second string: ");
-    scanf("%s", str2); // [cite: 81]
+    scanf("%s", str2); 
 
     while (1) {
-        // Menu Driven Interface [cite: 74, 76]
+        
         printf("\n--- String Operations Menu ---\n");
         printf("1. Length Calculation (strlen)\n");
         printf("2. String Copy (strcpy)\n");
@@ -23,26 +23,25 @@ int main() {
 
         switch (choice) {
             case 1:
-                // strlen() calculates length [cite: 73]
+               
                 printf("Length of first string: %lu\n", strlen(str1));
                 printf("Length of second string: %lu\n", strlen(str2));
                 break;
 
             case 2:
-                // strcpy() copies one string to another [cite: 73]
-                strcpy(temp, str1);
+                
                 printf("First string copied to temp: %s\n", temp);
                 break;
 
             case 3:
-                // strcat() joins two strings [cite: 73, 84]
-                strcpy(temp, str1); // Use temp to keep original str1 intact
+                
+                strcpy(temp, str1); 
                 strcat(temp, str2);
                 printf("Concatenated string: %s\n", temp); 
                 break;
 
             case 4:
-                // strcmp() compares two strings [cite: 73]
+               
                 if (strcmp(str1, str2) == 0)
                     printf("Strings are equal.\n");
                 else
