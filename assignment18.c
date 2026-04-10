4f8d7a63fbe7527d3ab371dfd9ce113f50f46d16
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:34:46 +0000

    drtghedh

--- assignment18.c
+++ assignment18.c
@@ -5,32 +5,31 @@ int main() {
     char str1[100], str2[100], temp[100];
     int length, i, j, isPalindrome = 1;
 
-    // INPUT: Accept strings from the user
-    printf("Enter a string: ");
-    gets(str1); // Using gets to accept strings with spaces
     
+    printf("Enter a string: ");
+    gets(str1); 
     printf("Enter another string for equality and substring check: ");
     gets(str2);
 
-    // i) Calculate length of string
+    
     length = strlen(str1);
     printf("\nLength of string = %d", length);
 
-    // ii) String reversal
-    strcpy(temp, str1); // Copy to temp to preserve original for other checks
+    
+    strcpy(temp, str1); 
     printf("\nReversed string = ");
     for (i = length - 1; i >= 0; i--) {
         printf("%c", temp[i]);
     }
 
-    // iii) Equality check
+    
     if (strcmp(str1, str2) == 0) {
         printf("\nStrings are equal");
     } else {
         printf("\nStrings are not equal");
     }
 
-    // iv) Check palindrome
+    
     for (i = 0; i < length / 2; i++) {
         if (str1[i] != str1[length - 1 - i]) {
             isPalindrome = 0;
@@ -43,7 +42,7 @@ int main() {
         printf("\nThe string is not a Palindrome");
     }
 
-    // v) Check substring
+    
     if (strstr(str1, str2) != NULL) {
         printf("\nSubstring found in main string\n");
     } else {
