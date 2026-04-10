162a6e51429416ddb968301d58700b13f3418f9f
rajneesh310 <bandwalrajneesh@gmail.com>
2026-04-10 05:27:23 +0000

    ghkcg

--- assignment6.c
+++ assignment6.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Recursive function
+
 int factorial_recursive(int n) {
     if (n == 0 || n == 1)
         return 1;  // Base case
@@ -8,7 +8,7 @@ int factorial_recursive(int n) {
         return n * factorial_recursive(n - 1);
 }
 
-// Iterative function
+
 int factorial_iterative(int n) {
     int i, fact = 1;
     for (i = 1; i <= n; i++) {
